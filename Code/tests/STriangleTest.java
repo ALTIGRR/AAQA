0fe8d6e9b05af6bf37364b705bb9c08d80a5da89
ALTIGRR <ALTIGRR@yandex.ru>
2025-10-11 23:16:47 +0300

    Junit_5

--- Code/tests/STriangleTest.java
+++ Code/tests/STriangleTest.java
@@ -5,10 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Площадь треугольника")
 
-public class sTriangleTest {
+public class STriangleTest {
     @Test
     public void testSTriangle() {
-        sTriangle area = new sTriangle(20, 15);
+        STriangle area = new STriangle(20, 15);
         assertEquals(150, area.area());
         System.out.println("Площадь треугольника вычисляется верно");
     }
