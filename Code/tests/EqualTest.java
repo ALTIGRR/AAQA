2b792323d97e1cd348d8df7e504c1b0367d18fd6
ALTIGRR <ALTIGRR@yandex.ru>
2025-10-13 14:23:35 +0300

    Update EqualTest.java

--- Code/tests/EqualTest.java
+++ Code/tests/EqualTest.java
@@ -2,19 +2,19 @@ import org.testng.annotations.Test;
 import org.testng.Assert;
 
 public class EqualTest {
-    @Test(testName = "Сравнение чисел")
+    @Test(testName = "Сравнение чисел 1")
     public void testEq1() {
         Equal y = new Equal();
         Assert.assertEquals(y.equals(5, 5), "x == z");
     }
 
-    @Test
+    @Test(testName = "Сравнение чисел 2")
     public void testEq2() {
         Equal y = new Equal();
         Assert.assertEquals(y.equals(10, 7), "x > z");
     }
 
-    @Test
+    @Test(testName = "Сравнение чисел 3")
     public void testEq3() {
         Equal y = new Equal();
         Assert.assertEquals(y.equals(13, 21), "x < z");
